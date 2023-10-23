98aaca3abf99dee40ade29ec9f2e74f1443909a6
Pratham Mittal <mittalpra101@gmail.com>
2023-10-23 11:33:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//images =5 (0,1,2,3,4)
+
+
+let flag=0;
+
+function controller(x){
+    flag+=x;
+    
+    slideshow(flag);
+}
+
+slideshow(flag);
+function slideshow(num){
+    let slides=document.getElementsByClassName('slide');    
+
+    if(num==slides.length){
+        flag=0;
+        num=0;
+    }
+    if(num<0){
+        flag=slides.length-1;
+        num=slides.length-1;
+    } 
+
+    for(let i of slides){
+        i.style.display="none";
+    }
+
+    slides[num].style.display="block";    
+}
+
